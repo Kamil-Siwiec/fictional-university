5942be5e626d670aa53b278f637de65a829ddfcf
Kamil Siwiec <kamsiwcode@gmail.com>
2022-06-14 15:46:54 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<p>Greetings from footer.php</p>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php wp_head(); ?>
+</head>
+
+
+
+<body>
+    <h1>Fictional University</h1>
+
+
+
+    
