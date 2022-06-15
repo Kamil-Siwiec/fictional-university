48a5cc931f0e44f2573b6bf9c883103c29b4f090
Kamil Siwiec <kamsiwcode@gmail.com>
2022-06-15 10:18:29 +0200

    feat: add font awesome icons

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files() {
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"');
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
