f5ffb8414fa66a6dc3781da86f3ce7f453a4bf80
Kamil Siwiec <kamsiwcode@gmail.com>
2022-06-15 10:09:18 +0200

    feat: modified file functions.php & removes styles from styles.css

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
