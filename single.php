3a6d5bdedb335cc6f9d96fa5b42674bd90504fc0
Kamil Siwiec <kamsiwcode@gmail.com>
2022-06-20 10:45:18 +0200

    feat: removed all files & modified as a new

--- single.php
+++ single.php
@@ -1,14 +1,14 @@
 <?php
+  
+  get_header();
 
-get_header();
+  while(have_posts()) {
+    the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    
+  <?php }
 
-while(have_posts()) {
-the_post(); ?>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
-
-<?php }
-
-get_footer();
+  get_footer();
 
 ?>
--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 
 // Instantiate a new object using our modules/classes
-const mobileMenu = new MobileMenu()
-const heroSlider = new HeroSlider()
+var mobileMenu = new MobileMenu()
+var heroSlider = new HeroSlider()
