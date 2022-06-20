508e1366cf672cd565040216778192909efc55eb
Kamil Siwiec <kamsiwcode@gmail.com>
2022-06-20 12:35:16 +0200

    feat: update page php - add if statement

--- page.php
+++ page.php
@@ -17,9 +17,20 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+<?php
+$theParent = wp_get_post_parent_id(get_the_ID());
+if ($theParent) { ?>
+  
+  <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
+<?php }
+?>
+
+
+
+
+    
     
     <!--
     <div class="page-links">
